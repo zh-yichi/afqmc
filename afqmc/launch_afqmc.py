19826226698679d86824b5f6da1d6348790eb090
yichi <zhangych98@gmail.com>
2026-05-22 15:58:13 -0700

    separate fp ph

--- afqmc/launch_afqmc.py
+++ afqmc/launch_afqmc.py
@@ -3,50 +3,56 @@ os.environ.setdefault("XLA_PYTHON_CLIENT_ALLOCATOR", "platform")
 
 import pickle
 
-def run_afqmc(options,
-              option_file='options.bin',
-              script=None,
-              ):
+def ph_afqmc(
+        options,
+        script=None,
+        option_file='options.bin',
+        ):
 
     with open(option_file, 'wb') as f:
         pickle.dump(options, f)
-    
-    # if options["use_gpu"]:
-        # config.afqmc_config["use_gpu"] = True
-        # config.setup_jax()
-    #     gpu_flag = "--use_gpu"
-    # else:
-    #     gpu_flag = ""
 
     if script is None:
-        if options["free_projection"]:
-            if  'pt2' in options['trial']:
-                script='run_fpafqmc_pt2.py'
+        if  'pt' in options['trial']:
+            if '2' in options['trial']:
+                script='run_afqmc_pt2ccsd.py'
             else:
-                script = 'run_fpafqmc.py'
+                script='run_afqmc_ptccsd.py'
+        
+        elif 'sto' in options['trial']:
+            if '2' in options['trial']:
+                script='run_afqmc_stoccsd2.py'
+            else:
+                script='run_afqmc_stoccsd.py'
 
         else:
-            if  'pt' in options['trial']:
-                if '2' in options['trial']:
-                    script='run_afqmc_pt2ccsd.py'
-                else:
-                    script='run_afqmc_ptccsd.py'
-            
-            elif 'sto' in options['trial']:
-                if '2' in options['trial']:
-                    script='run_afqmc_stoccsd2.py'
-                else:
-                    script='run_afqmc_stoccsd.py'
-
-            else:
-                script='run_afqmc.py'
+            script='run_afqmc.py'
 
     path = os.path.abspath(__file__)
     dir_path = os.path.dirname(path)
     script = f"{dir_path}/scripts/{script}"
     print(f'QMC script: {script}')
 
-    os.system(
-        # f" python {script} {gpu_flag} |tee afqmc.out"
-        f" python {script} |tee afqmc.out"
-    )
+    os.system(f" python {script} |tee afqmc.out")
+
+def fp_afqmc(
+        options,
+        script=None,
+        option_file='options.bin',
+        ):
+
+    with open(option_file, 'wb') as f:
+        pickle.dump(options, f)
+
+    if script is None:
+        if  'pt2' in options['trial']:
+            script='run_fpafqmc_pt2.py'
+        else:
+            script = 'run_fpafqmc.py'
+
+    path = os.path.abspath(__file__)
+    dir_path = os.path.dirname(path)
+    script = f"{dir_path}/scripts/{script}"
+    print(f'QMC script: {script}')
+
+    os.system(f" python {script} |tee afqmc.out")
